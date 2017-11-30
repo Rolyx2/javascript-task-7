53c01ef3c08bf2fd2b87062c01d5f3e56ce9fd0d
Maxim <32168435+Rolyx2@users.noreply.github.com>
2017-11-30 16:10:31 +0500

    ?

--- async.js
+++ async.js
@@ -3,26 +3,21 @@
 exports.isStar = true;
 exports.runParallel = runParallel;
 
-/**
-* @param {Array} jobs – функции, которые возвращают промисы
-* @param {Number} parallelNum - число одновременно исполняющихся промисов
-* @param {Number} timeout - таймаут работы промиса
-* @returns {Array}
-*/
-
 function runParallel(jobs, parallelNum, timeout = 1000) {
     return new Promise((resolve) => {
         if (!jobs.length) {
             resolve([]);
         }
-        let numberOfWork = 0;
-        let results = [];
+
+        var numberOfWork = 0;
+        var results = [];
+
         for (var i = 0; i < parallelNum; i++) {
             start(jobs[numberOfWork], numberOfWork++);
         }
 
         function start(job, index) {
-            let answer = result => finish(result, index);
+            var answer = result => finish(result, index);
             new Promise((resolveJob, rejectJob) => {
                 job().then(resolveJob, rejectJob);
                 setTimeout(rejectJob, timeout, new Error('Error'));
